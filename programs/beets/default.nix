1f74f92cd30ecf3fc4204be9eef9569252063daa
Tobias Schmalz <github@choreutes.de>
2021-07-06 17:21:33 +0200

    Adjust beets and htop config for new home-manager release

--- programs/beets/default.nix
+++ programs/beets/default.nix
@@ -1,7 +1,27 @@
 { config, pkgs, ... }:
 
 {
-  programs.beets.enable = true;
+  programs.beets = {
+    enable = true;
 
-  xdg.configFile."beets/config.yaml".source = ./config.yaml;
+    settings = {
+      directory = "~/Musik";
+      library = "~/.local/share/beets/library.db";
+
+      ui.color = "yes";
+
+      import = {
+        move = "yes";
+        write = "no";
+      };
+
+      paths = {
+        default = "$genre/$albumartist/$album%aunique{}/$track - $title";
+        "genre:Ballroom" = "Ballroom/$album%aunique{}/$track - $title ($comments)";
+        "genre:Soundtrack" = "Soundtracks/$album%aunique{}/$track - $title";
+      };
+    };
+  };
+
+  # xdg.configFile."beets/config.yaml".source = ./config.yaml;
 }
