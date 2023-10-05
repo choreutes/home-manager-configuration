a5cb02357c2e31801c52b84b5ba4fd8f9f11ce48
Tobias Schmalz <github@choreutes.de>
2023-10-05 16:17:54 +0200

    First try at forwarding gpg agent via ssh

--- services/gnupg/default.nix
+++ services/gnupg/default.nix
@@ -15,8 +15,20 @@
     };
   };
 
+  programs.ssh = {
+    enable = true;
+
+    extraConfig = ''
+      Host opal.choreutes.de
+        RemoteForward /run/user/1000/gnupg/S.gpg-agent /run/user/1000/gnupg/S.gpg-agent.extra
+        RemoteForward /run/user/1000/gnupg/S.gpg-agent.ssh /run/user/1000/gnupg/S.gpg-agent.ssh
+    '';
+  };
+
   services.gpg-agent = {
     enable = true;
+
+    enableExtraSocket = true;
 
     enableSshSupport = true;
     sshKeys = [ "178D43591E8A412D0AA5098567E5AEF509BB3C0A" ];
