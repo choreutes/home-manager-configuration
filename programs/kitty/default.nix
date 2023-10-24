1de289dce98270a6f4eb3523174c20a2c9d46b42
Tobias Schmalz <github@choreutes.de>
2023-10-24 17:40:32 +0200

    Switch to home-manager for kitty configuration

--- programs/kitty/default.nix
+++ programs/kitty/default.nix
@@ -1,13 +1,31 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    kitty
-  ];
+  programs.kitty = {
+    enable = true;
 
-  xdg.configFile = {
-    "kitty/kitty.conf".source = ./kitty.conf;
-    "kitty/colorschemes/papercolor-dark.conf".source = ./colorschemes/papercolor-dark.conf;
-    "kitty/colorschemes/papercolor-light.conf".source = ./colorschemes/papercolor-light.conf;
+    font = {
+      name = "SauceCodePro Nerd Font";
+      size = 10;
+    };
+
+    settings = {
+      background_opacity = "0.8";
+      enable_audio_bell = false;
+    };
+
+    theme = "PaperColor Dark";
   };
+
+  # Old configuration method. Keep around for reference/backup
+  #
+  # home.packages = with pkgs; [
+  #   kitty
+  # ];
+
+  # xdg.configFile = {
+  #   "kitty/kitty.conf".source = ./kitty.conf;
+  #   "kitty/colorschemes/papercolor-dark.conf".source = ./colorschemes/papercolor-dark.conf;
+  #   "kitty/colorschemes/papercolor-light.conf".source = ./colorschemes/papercolor-light.conf;
+  # };
 }
